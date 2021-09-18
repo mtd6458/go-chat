ac280c58e40835a734cfac6b4dae513ca16bf90a
mtd6458 <md.yu.03@gmail.com>
2021-09-18 12:55:51 +0900

    update 参加、退室、全てのクライアントにメッセージを送信するメソッドを定義

--- room.go
+++ room.go
@@ -11,3 +11,28 @@ type room struct {
 	clients map[*client]bool
 }
 
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			//退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// 全てのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					//メッセージを送信
+				default:
+					//送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
