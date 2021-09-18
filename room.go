dfa82c0e7104c243c62c9854487922f49490c115
mtd6458 <md.yu.03@gmail.com>
2021-09-18 12:37:46 +0900

    update 同時アクセスによる競合アクセスを防ぐ為に変数追加

--- room.go
+++ room.go
@@ -3,5 +3,11 @@ package main
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
 	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントの為のチャネルです。
+	join chan *client
+	// leaveはチャットルームから体質しようとしているクライアントの為のチャネルです。
+	leave chan *client
+	// clientsには在室している全てのクライアントが保持されます。
+	clients map[*client]bool
 }
 
