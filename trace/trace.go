bd7472e7b722edbaec24bb1354adb1f580fba2c0
mtd6458 <md.yu.03@gmail.com>
2021-09-23 01:34:20 +0900

    update テスト実装

--- trace/trace.go
+++ trace/trace.go
@@ -1,7 +1,31 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 // Tracer Tracerはコード内での出来事を記録できるオブジェクトを表すインターフェースです。
 // ...Interface{}という型は、任意の型の引数を何個でも受け取ることができる。(０個でもOK
 type Tracer interface {
 	Trace(...interface{})
+}
+
+type tracer struct {
+	// 情報出力用のフィールド
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	if _, err := t.out.Write([]byte(fmt.Sprint(a...))); err != nil {
+		return
+	}
+
+	if _, err := t.out.Write([]byte("\n")); err != nil {
+		return
+	}
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
 }
--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -1,10 +1,20 @@
 package trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 // t *testing.T型の引数を一つ受け取る関数は全てユニットテストとみなされる。
-func TestNew(t *testing.T)  {
-
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは")
+		if buf.String() != "こんにちは\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
 }
